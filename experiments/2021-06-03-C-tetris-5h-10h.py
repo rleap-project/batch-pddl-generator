5af14dc5d491f78b70404ad944ee925e49e12a07
Jendrik Seipp <jendrik.seipp@liu.se>
2021-06-08 12:58:13 +0200

    Add experiments.

---
+++ experiments/2021-06-03-C-tetris-5h-10h.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import project
+
+
+DOMAINS = [
+    "tetris",
+]
+ATTRIBUTES = [
+    project.Attribute("final_value", min_wins=True),
+    project.Attribute("evaluated_configurations", min_wins=False),
+    project.Attribute("wallclock_time", min_wins=None),
+    project.Attribute("incumbent_changed", min_wins=False),
+    project.Attribute("evaluation_time", min_wins=False),
+    "error", "run_dir", "final_*", "smac_exit_code",
+]
+SMAC_RUNS_PER_DOMAIN = 50
+EXTRA_OPTIONS = []
+
+if not project.REMOTE:
+    SMAC_RUNS_PER_DOMAIN = 1
+    EXTRA_OPTIONS += ["--debug", "--max-configurations", "3"]
+
+exp = project.get_smac_experiment(DOMAINS, SMAC_RUNS_PER_DOMAIN, ATTRIBUTES, EXTRA_OPTIONS)
+exp.run_steps()
