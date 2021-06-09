2e82b6a958d616fedd06f168125abb6fdc6e99d9
Jendrik Seipp <jendrik.seipp@liu.se>
2021-06-09 15:04:59 +0200

    Skip duplicate tasks.

--- src/collect-instances.py
+++ src/collect-instances.py
@@ -2,6 +2,7 @@
 
 import argparse
 from collections import defaultdict
+import hashlib
 import json
 from pathlib import Path
 import shutil
@@ -18,6 +19,18 @@ def parse_args():
     parser.add_argument("expdir", help="Experiment directory")
     parser.add_argument("destdir", help="Destination directory for benchmarks")
     return parser.parse_args()
+
+
+def _compute_md5_hash(s):
+    m = hashlib.md5()
+    m.update(s.encode("utf-8"))
+    return m.hexdigest()
+
+
+def hash_task(plan_dir):
+    with open(plan_dir / "problem.pddl") as f:
+        content = f.read()
+    return _compute_md5_hash(content)
 
 
 def record_max_values(parameters, max_domain_values):
@@ -41,6 +54,7 @@ def main():
     destdir = Path(args.destdir)
     plan_dirs = expdir.glob("runs-*-*/*/smac-*/run_*/plan/*/*/")
     max_values = defaultdict(dict)
+    seen_task_hashes = defaultdict(set)
     for plan_dir in plan_dirs:
         try:
             with open(plan_dir / "properties.json") as f:
@@ -50,13 +64,23 @@ def main():
         if props["planner_exitcode"] != 0:
             continue
         print(f"Found {props}")
+        domain = props["domain"]
+
+        # Skip duplicate tasks.
+        hash = hash_task(plan_dir)
+        if hash in seen_task_hashes[domain]:
+            print("Skip duplicate task")
+            continue
+        else:
+            seen_task_hashes[domain].add(hash)
+
         values = props["parameters"].copy()
         values["planner_runtime"] = props["runtime"]
-        record_max_values(values, max_values[props["domain"]])
+        record_max_values(values, max_values[domain])
         parameters = utils.join_parameters(props["parameters"])
         seed = props["seed"]
         problem_name = f"p-{parameters}-{seed}.pddl"
-        target_dir = destdir / props["domain"]
+        target_dir = destdir / domain
         target_dir.mkdir(parents=True, exist_ok=True)
         shutil.copy2(plan_dir / "problem.pddl", target_dir / problem_name)
 
