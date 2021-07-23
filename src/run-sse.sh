60c17e98b9c148c399316cba421954472f8175ef
Jendrik Seipp <jendrik.seipp@liu.se>
2021-07-23 12:36:50 +0200

    Remove tmp FS dir after running SSE.

--- src/run-sse.sh
+++ src/run-sse.sh
@@ -10,12 +10,17 @@ fi
 SSE="$1"
 DOMAIN="$2"
 PROBLEM="$3"
+FSTMPDIR=fs-tmp
 
 export LANG=C
 
 /usr/bin/time -o /dev/stdout -f "SSE runtime: %es real, %Us user, %Ss sys" \
-  "$SSE" \
+  singularity run \
+    "$SSE" \
     --workspace "$(pwd)" \
+    --tag "$FSTMPDIR" \
     --domain "$DOMAIN" \
     --instance "$PROBLEM" \
     --options "ignore_non_fringe_dead_states=0"
+
+rm -rf "$FSTMPDIR"
